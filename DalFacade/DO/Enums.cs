b404bbf432a855aa3bd02a7855acaa53472cbb29
נועה זינגר < @mbjcomp.local>
2022-10-24 15:02:15 +0300

    enumn

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace DO
-{
-    internal class Enums
-    {
-    }
+namespace DalFacade.DO;
+
+public enum catagory{
+    suit,
+    pents,
+    tie,
+    shirt,
+
 }
+    
+    
+
