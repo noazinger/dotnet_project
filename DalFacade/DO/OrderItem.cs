426825b13f4168d009b72e11def80e12ea37f426
t0583204001@gmail.com <ToviSchindler>
2022-10-30 17:29:54 +0200

    add toString function

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -8,9 +8,17 @@ namespace DO;
 
     public struct OrderItem
     {
-    int ProductID;
-    int OrderID;
-    double Price;
-    int Amount;
+    int ProductID { set; get;}
+    int OrderID { set; get;};
+    double Price { set; get;};
+    int Amount { set; get;};
+
+    public override string ToString() => $@"
+        Product ID={ProductID}, 
+        Order ID = {OrderID},
+    	Price= {Price},
+    	Amount= {Amount}
+";
+
 }
 
