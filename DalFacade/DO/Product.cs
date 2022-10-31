baf46d257ecd7e2219805870582cc5fbc20ec3c8
t0583204001@gmail.com <ToviSchindler>
2022-10-31 15:42:15 +0200

    data source changes

--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -1,8 +1,13 @@
-﻿using DO;
-namespace Dal.DO;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
 
 
-internal struct Product
+public struct Product
 {
     int ID;
     string Name;
