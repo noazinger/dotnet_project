fe3823feb6b700923229cc94cc8422e4d03f0147
t0583204001@gmail.com <ToviSchindler>
2022-11-02 16:24:28 +0200

    finish createOrderData func

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -8,10 +8,10 @@ namespace DO;
 
     public struct OrderItem
     {
-    int ProductID { set; get;}
-    int OrderID { set; get;}
-    double Price { set; get;}
-    int Amount { set; get;}
+    public int ProductID { set; get;}
+    public int OrderID { set; get;}
+    public double Price { set; get;}
+    public int Amount { set; get;}
 
     public override string ToString() => $@"
         Product ID={ProductID}, 
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -1,4 +1,5 @@
 ﻿using DO;
+using System;
 namespace Dal;
 
     internal struct DataSource
@@ -6,9 +7,10 @@ namespace Dal;
     private static void s_Initialize()
     {
         createProductData();
+        //createOrderData(i);
     }
-   
-      
+
+
     public static (catagory, string)[] TupleProduct = new[] {
         (catagory.suit, "black_suit"), (catagory.pents, "boy_pent"),
         (catagory.tie, "patterned_ties"),(catagory.shirt, "long_shirt"),
@@ -17,10 +19,11 @@ namespace Dal;
         (catagory.tie, "grooms_tie"),(catagory.accssories, "swarovski_cufflinks")
     };
     static DataSource() { s_Initialize(); }
+
     static Random readionly = new Random();
     public static OrderItem[] orderItemsArr = new OrderItem[200];
     public static Order[] ordersData = new Order[100];
-   public static Product[] productsData=new Product[50];
+    public static Product[] productsData=new Product[50];
     public static void createProductData()
     {
       for(int i = 0; i < 12; i++)
@@ -37,13 +40,21 @@ namespace Dal;
 
     public static void createOrderData(int ID)
     {
+        string[] CustomerName = { "Noa", "Tovi", "Gitty" };
+        string[] CustomerEmail = { "Noa123@gmail.com", "Tovi40@gmail.com", "Gitty865@gmail.com" };
+        string[] CustomerAdress = { "Noa123@gmail.com", "Tovi40@gmail.com", "Gitty865@gmail.com" };
         for (int i = 0; i < 22; i++)
         {
             ordersData[i] = new Order();
             ordersData[i].ID = Config.OrderIndex++;
-            ordersData[i].ShipDate = DateTime.Now;
-            ordersData[i].OrderDate = DateTime.Now;
-            ordersData[i].DeliveryDate = DateTime.Now;        
+            ordersData[i].CustomerName = CustomerName[(int)readionly.NextInt64(CustomerName.Length)];
+            ordersData[i].CustomerEmail = CustomerEmail[(int)readionly.NextInt64(CustomerEmail.Length)];
+            ordersData[i].CustomerAdress = CustomerAdress[(int)readionly.NextInt64(CustomerAdress.Length)];
+            ordersData[i].OrderDate = DateTime.MinValue;
+            TimeSpan delivery = TimeSpan.FromDays(2);
+            ordersData[i].DeliveryDate = ordersData[i].OrderDate + delivery;
+            TimeSpan shipDays = TimeSpan.FromDays(7);
+            ordersData[i].ShipDate = ordersData[i].DeliveryDate + shipDays;
         };
     }
     public class Config
