f1fdc79859786069ca87feade5148c95989f74b4
t0583204001@gmail.com <ToviSchindler>
2022-11-02 18:14:23 +0200

    dalProduct

--- DalList/DalProduct.cs
+++ DalList/DalProduct.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DO;
 namespace Dal
 {
     internal struct DalProduct
     {
+        public static void Create(Product obj)
+        {
+            if (DataSource.Config.ProductIndex > 49)
+            {
+                throw new Exception("There isn't enough space");
+            }
+            DataSource.productsData[DataSource.Config.ProductIndex++] = obj;
+        }
+        public static Product ReadSingle(int Id)
+        {
+            if (Id < 100000 || Id > 999999)
+            {
+                throw new Exception("ID isn't valid");
+            }
+            for(int i = 0; i < DataSource.Config.ProductIndex; i++)
+            {
+                if (DataSource.productsData[i].ID == Id)
+                    return DataSource.productsData[i];
+            }
+            throw new Exception("The product isn't exist");
+        }
+
+        public static Product[] Read()
+        {
+            Product[] tempProducts = new Product[DataSource.Config.ProductIndex];
+            for(int i = 0; i < DataSource.Config.ProductIndex; i++)
+            {
+                tempProducts[i] = DataSource.productsData[i];
+            }
+            return tempProducts;
+        }
+
+        public static void Delete(int Id)
+        {
+            for (int i = 0; i < DataSource.Config.ProductIndex; i++)
+            {
+                if(DataSource.productsData[i].ID == Id)
+                {
+                    Product temp=DataSource.productsData[i];
+                    DataSource.productsData[i]= DataSource.productsData[DataSource.Config.ProductIndex];
+                    DataSource.productsData[DataSource.Config.ProductIndex]= temp;
+                    DataSource.Config.ProductIndex--;
+                }
+            }
+            throw new Exception("Product isn't exist");
+        }
+
+        public static void UpDate(Product obj)
+        {
+            for (int i = 0; i < DataSource.Config.ProductIndex; i++)
+            {
+                if (DataSource.productsData[i].ID == obj.ID)
+                {
+                    DataSource.productsData[i] = obj;
+                }
+            }
+            throw new Exception("Product isn't exist");
+        }
     }
 }
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -7,7 +7,8 @@ namespace Dal;
     private static void s_Initialize()
     {
         createProductData();
-        //createOrderData(i);
+        createOrderData();
+        createOrderItemData();
     }
 
 
@@ -19,7 +20,6 @@ namespace Dal;
         (catagory.tie, "grooms_tie"),(catagory.accssories, "swarovski_cufflinks")
     };
     static DataSource() { s_Initialize(); }
-
     static Random readionly = new Random();
     public static OrderItem[] orderItemsData = new OrderItem[200];
     public static Order[] ordersData = new Order[100];
@@ -35,11 +35,12 @@ namespace Dal;
             productsData[i].catagory = TupleProduct[Irnd].Item1;
             productsData[i].Name = TupleProduct[Irnd].Item2;
             productsData[i].Price = readionly.Next(100, 650);
-            productsData[i].inStock= readionly.Next(0, 200);//לאתחל 5 אחוז מהמוצרים ב-0\        
+            productsData[i].inStock= readionly.Next(0, 200);//לאתחל 5 אחוז מהמוצרים ב-0\
+            Config.ProductIndex++;
         };
     }
 
-    public static void createOrderData(int ID)
+    public static void createOrderData()
     {
         string[] CustomerName = { "Noa", "Tovi", "Gitty" };
         string[] CustomerEmail = { "Noa123@gmail.com", "Tovi40@gmail.com", "Gitty865@gmail.com" };
@@ -81,6 +82,7 @@ namespace Dal;
         static public int OrderIndex = 0;
         static public int OrderiTemIndex = 0;
         static public int OrderFinalIndex = 100;
+        static public int ProductIndex = 0;
         internal int getOrderFinalIndex() { return OrderFinalIndex++; }
 
 
