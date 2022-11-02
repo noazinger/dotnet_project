96be4f942adad4a52a5845432c31107ec8176b1a
t0583204001@gmail.com <ToviSchindler>
2022-11-02 12:14:22 +0200

    add ProductListFunction

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -7,15 +7,17 @@ namespace Dal;
     OrderItem[] orderItemsArr = new OrderItem[200];
     Order[] ordersArr = new Order[100];
     Product[] productsArr=new Product[50];
-    int[] ProductsPrice = {200,100,50,40,300,43,450,70,20,120,550};
-    int[] Amount = { 7, 98, 40, 104, 30, 65, 0, 0, 80, 30, 10, 5, 6, 15, 54 };
-    Object[] productName = { new { catagory="suit", name="boySuit" },
-       new { catagory="pents", name="boyPent" },
-       new { catagory="tie", name="patternedTies" },
-       new { catagory="shirt", name="longShirt" },
-       new { catagory="accssorie", name="cufflinks" },
-
-
-    };
+    public static void createProductList()
+    {
+        
+    }
+    //int[] ProductsPrice = {200,100,50,40,300,43,450,70,20,120,550};
+    //int[] Amount = { 7, 98, 40, 104, 30, 65, 0, 0, 80, 30, 10, 5, 6, 15, 54 };
+    //Object[] productName = { new { catagory="suit", name="boySuit" },
+    //   new { catagory="pents", name="boyPent" },
+    //   new { catagory="tie", name="patternedTies" },
+    //   new { catagory="shirt", name="longShirt" },
+    //   new { catagory="accssorie", name="cufflinks" },
+    //};
 }
 
