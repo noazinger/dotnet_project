53e238a264261878a4d4449625d94f49859d5354
t0583204001@gmail.com <ToviSchindler>
2022-11-13 17:14:26 +0200

    Interface Icrud

---
+++ DalFacade/DalApi/Icrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace DalApi
+{
+    internal interface Icrud
+    {
+        void Create <T> (T n);
+        void Update<T> (T n);
+        IEnumerable <T> Read<T> (T n);
+        T ReadSingle<T> (T n);
+        void Delete<T> (T n);
+        
+    }
+}
