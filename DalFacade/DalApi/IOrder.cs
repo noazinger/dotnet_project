f23ed65bf724805e49ed691d5dc8fb272b06fe27
זינגר נועה < @J25>
2022-11-13 18:43:07 +0200

    .ם

--- DalFacade/DalApi/IOrder.cs
+++ DalFacade/DalApi/IOrder.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DO;
 namespace DalApi
 {
-    public interface IOrder
+    public interface IOrder : Icrud<Order>
     {
+
     }
 }
