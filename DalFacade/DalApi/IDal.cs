36edd69df62d68f4d6559ab47f95310eec2e9fb2
t0583204001@gmail.com <ToviSchindler>
2022-11-16 12:51:31 +0200

    add Idal

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+namespace DalApi
+{
+    internal class IDal
+    {
+        Order _order { get;  }
+        Product _product { get; }
+        OrderItem _orderItem { get;}
+    }
+}
