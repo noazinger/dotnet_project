1e0e37d316b1003ae6e8ec6c14fdd4eaa80d8860
t0583204001@gmail.com <ToviSchindler>
2022-11-21 14:19:15 +0200

    add claa productForList

---
+++ Bl/BO/ProductForList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+namespace BO
+{
+    public class ProductForList
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public catagory catagory { get; set; }
+
+        public override string ToString() => $@"
+        ID={ID},
+        Product Name={Name}, 
+        Product Price={Price},
+        Product catagory={catagory},
+        ";
+    }
+}
