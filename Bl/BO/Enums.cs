c03b189682b36e0fad82d9b5127fe1965ed3b3b1
t0583204001@gmail.com <ToviSchindler>
2022-11-21 14:25:14 +0200

    enums

--- Bl/BO/Enums.cs
+++ Bl/BO/Enums.cs
@@ -10,5 +10,26 @@ namespace BO
     {
       Dispatched, Delayed, Delivered
     }
+    public enum catagory
+    {
+        suit,
+        pants,
+        tie,
+        shirt,
+        accssories
+    };
+    public enum productsNames
+    {
+        boy_pent,
+        patterned_ties,
+        long_shirt,
+        silver_cufflinks,
+        cotton_suit,
+        short_Shirt,
+        belt,
+        grooms_tie,
+        black_suit,
+        cotton_shirt
+    };
 }
     
