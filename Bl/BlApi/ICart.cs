c624eb40de69b2e504668ec93a002c365917fc29
t0583204001@gmail.com <ToviSchindler>
2022-11-21 15:01:41 +0200

    add interface class

---
+++ Bl/BlApi/ICart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    public  interface ICart
+    {
+        public void Create(ICart cart);
+        public void Update(ICart cart);
+
+    }
+}
