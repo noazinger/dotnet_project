28de1f350fa8f0e2becd0fbdd0cdfd6198713ef6
זינגר נועה < @J25>
2022-11-22 15:06:08 +0200

    start Blproduct

---
+++ Bl/BlImplementation/BlProduct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  BlApi;
+using DalApi;
+namespace BlImplementation
+{
+    internal class BlProduct:Iproduct
+    {
+        IDal dalEntity = new Dal.DalList();
+        public IEnumerable<Iproduct> ReadListProducts()
+        {
+           
+            foreach(DO.Product item in dalEntity.Product.Read())
+            {
+                BO.Product(item.ID, item.Name, item.inStock, item.catagory, item.Price);
+                BO.ProductForList.
+            }
+        }
+        public Iproduct ReadCatalog();
+        public Iproduct ReadSingleProductForDirector(int id);
+        public Iproduct ReadSingleProductForCustomer(int id);
+        public void Create(Iproduct product);
+        public void Delete(int id);
+        public void Update(Iproduct product);
+    }
+}
