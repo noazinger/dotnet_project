14e8b103a9d82de0243c99425de0375e4a903588
TOVI <t0583204001@gmail.com>
2022-11-23 01:21:53 +0200

    change readOrdersFunction

---
+++ BL/BlImplementation/BlOrder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+using BO;
+using DalApi;
+namespace BlImplementation
+{
+    internal class BlOrder : IOrder
+    {
+        IDal dalEntity = new Dal.DalList();
+        public IEnumerable<IOrder> ReadOrders()
+        {
+            List<OrderForList> ItemsList = new List<OrderForList>();
+            foreach (DO.Order item in dalEntity.Order.Read())
+            {
+                /*BO.Order()*/
+                ItemsList.Add(item);
+            }
+            return ItemsList;
+        }
+        public IOrder ReadOrderInformation(int id);
+        public IOrder UpdateShipping(int orderNumber);
+        public IOrder UpdateDelivery(int orderNumber);
+        public void Update(int orderNumber);
+    }
+}
