3006347e0a4709f185716a8b9075688d49f83401
זינגר נועה < @J25>
2022-11-23 19:35:58 +0200

    nnn

--- BL/BO/Exceptions.cs
+++ BL/BO/Exceptions.cs
@@ -6,24 +6,52 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-   
-        public class NotExistException : Exception
-        {
-        public NotExistException(Exception exc):base("The item does not exist", exc)
-        {
 
-        }
-       
-         
-
-        }
-    public class NotValidException : Exception
+    public class NotInStock : Exception
     {
+        public NotInStock() : base("The product does not in stock")
+        {
 
-        public override string Message => "  The ID number is incorrect";
-
+        }
+    }
+    public class NotExistException : Exception
+    {
+        public NotExistException(Exception exc) : base("The item does not exist", exc) { }
+        public NotExistException(string mess) : base(mess) { }
 
     }
-  
+    public class NotDataException : Exception
+    {
+
+        public NotDataException(Exception exc) : base("the Data Is Empty", exc)
+        {
+
+        }
+    }
+    public class AlreadyExistsException : Exception
+    {
+        public AlreadyExistsException(Exception exc) : base("it is already exists on data", exc)
+        {
+
+        }
+    }
+
+    
+    public class NotValidException : Exception
+    {
+      public NotValidException(Exception exc) : base("the input is not valid", exc) { }
+   
+        public NotValidException(string exc) : base(exc) { }
+     
+
+    }
+    public class ExistsInOrder : Exception
+    {
+        public ExistsInOrder() : base("the product exist in some order")
+        {
+
+        }
+    }
+
 
 }
