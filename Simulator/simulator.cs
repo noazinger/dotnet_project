d6296f933866caa9daef961fb58dab6a606a72fa
t0583204001@gmail.com <ToviSchindler>
2023-02-22 19:23:04 +0200

    simulatoe xaml

--- Simulator/simulator.cs
+++ Simulator/simulator.cs
@@ -1,14 +1,13 @@
 ﻿using BlApi;
 using BO;
-
-
-
-
-
 namespace Simulator
 {
     public static class simulator
     {
+
+
+
+
         static bool bContinue = true;
         private static readonly BlApi.IBl? bl = BlApi.Factory.Get();
         private static string previousStatus { get; set; }
@@ -16,36 +15,34 @@ namespace Simulator
         static Random random = new Random();
         public static event EventHandler newProcces;
         public static event EventHandler ProgressChange;
-
-
         public static void run()
         {
             new Thread(
          () =>
          {
-            while (bContinue)
-            {
-                int? OrderId= bl?.Order.SelectingOrderForTreatment();
-                if (OrderId == null) bContinue = false;
-                BO.Order? order = bl?.Order.ReadOrderInformation((int)OrderId);
+             while (bContinue)
+             {
+
+                 int? OrderId = bl?.Order.SelectingOrderForTreatment();
+                 if (OrderId == null) bContinue = false;
+                 BO.Order? order = bl?.Order.ReadOrderInformation((int)OrderId);
                  previousStatus = order.Status.ToString();
                  nextStatus = order.Status.ToString();
                  int time = random.Next(1000, 4000);
-
-                 CurruntOrder cOrder = new(order, time);
+                 if (previousStatus == "Dispatched")
+                 {
+                     order = bl?.Order.UpdateShipping((int)OrderId);
+                     nextStatus = order.Status.ToString();
+                 }
+                 else if (order?.DeliveryDate == DateTime.MinValue)
+                 {
+                     order = bl?.Order.UpdateDelivery((int)OrderId);
+                     nextStatus = order.Status.ToString();
+                 }
+                 CurruntOrder cOrder = new(previousStatus, nextStatus, time);
                  if (ProgressChange != null)
                      ProgressChange(null, cOrder);
-                Thread.Sleep(time * 1000);
-                if (previousStatus== "Dispatched")
-                {
-                     order=bl?.Order.UpdateShipping((int)OrderId);
-                     nextStatus = order.Status.ToString();
-                }
-                else if (order?.DeliveryDate == DateTime.MinValue)
-                {
-                    order=bl?.Order.UpdateDelivery((int)OrderId);
-                    nextStatus = order.Status.ToString();
-                }
+                 Thread.Sleep(time * 1000);
              }
          }
             ).Start();
@@ -53,11 +50,13 @@ namespace Simulator
     }
     public class CurruntOrder : EventArgs
     {
-        public Order order;
+        public string currentStatus;
+        public string nextStatus;
         public int seconds;
-        public CurruntOrder(Order ord, int sec)
+        public CurruntOrder(string currentSt,string nextSt, int sec)
         {
-            order = ord;
+            currentStatus= currentSt;
+            nextStatus= nextSt;
             seconds = sec;
         }
     }
